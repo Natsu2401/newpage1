f07bfe209741607b6717fc1c78cfeebb3e42d7ed
Natsu2401 <111423869+Natsu2401@users.noreply.github.com>
2024-01-29 21:31:56 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-sudo pacman -S neovim git bluez bluez-utils blueman
+ sudo pacman -S neovim git bluez bluez-utils blueman neovim ruby nodejs npm python-pip unzip zsh
 
 #Enable the Service
 sudo systemctl start bluetooth.service
@@ -16,3 +16,12 @@ curl -OJ 'https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=yay'
 makepkg -si
 cd
 rm -rf /tmp/yay
+
+#setup neovim 
+pip install neovim --break-system-packages
+sudo pip install pynvim --break-system-packages
+sudo npm install -g neovim
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
+
+#setup zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
