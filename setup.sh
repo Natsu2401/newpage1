715d5d204b50efdd88d4702e81f6afe28c2a344c
Natsu2401 <111423869+Natsu2401@users.noreply.github.com>
2024-01-29 20:19:18 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,18 @@
-sudo pacman -S neovim git 
+sudo pacman -S neovim git bluez bluez-utils blueman
+
+#Enable the Service
+sudo systemctl start bluetooth.service
+sudo systemctl enable bluetooth.service
+
+#Connecting Audio
+sudo pacman -S pulseaudio
+sudo pacman -S pulseaudio-bluetooth
+sudo pacman -S pavucontrol
+
+# install yay 
+mkdir /tmp/yay
+cd /tmp/yay
+curl -OJ 'https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=yay'
+makepkg -si
+cd
+rm -rf /tmp/yay
